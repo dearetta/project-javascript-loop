a81560cfd46081f8ad6f1567b9c284b84cd8ce28
dearettaa@gmail.com <dearettaa@gmail.com>
2019-06-25 13:42:36 +0700

    add md

---
+++ index.js
@@ -0,0 +1,27 @@
+let i = 1
+let iteration = 2 + 3
+const numbers = [2,3,4,5,6,7,8]
+do{
+    console.log(i);
+    i++
+    
+}while (i < 10)
+
+if(iteration == 5)
+{
+    for (i = 1; i < 10; i++){
+        console.log(`the result is ${iteration}`);
+        iteration++
+    }
+} else{
+    console.log(".....");
+    
+}
+
+for (let i = 0; i <10; i++){
+
+    let element = numbers[i]
+    console.log(element);
+    
+    
+}
